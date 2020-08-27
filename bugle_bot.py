642aa903bbeafdce9f07b214ebc1453ff782a858
Scipi <scipii48@gmail.com>
2020-08-26 21:35:16 -0400

    Implemented basic Hello World bot script

--- bugle_bot.py
+++ bugle_bot.py
@@ -1,8 +1,10 @@
 """Main entry file"""
+import bugle_bot
+import bugle_bot.config
 
 
 def main():
-    pass
+    bugle_bot.client.run(bugle_bot.config.DISCORD_TOKEN)
 
 
 if __name__ == '__main__':
